7fe232ec360a83a9b10699cc3152a4365e03cfb1
Agustinus Wikrama <agustinuswikrama@gmail.com>
2024-12-31 14:48:43 +0700

    chore: initial commit

---
+++ src/hooks/useBle.ts
@@ -0,0 +1,144 @@
+import {useState} from 'react';
+import {PermissionsAndroid, ToastAndroid} from 'react-native';
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+} from 'react-native-ble-plx';
+import DeviceInfo from 'react-native-device-info';
+import {PERMISSIONS} from 'react-native-permissions';
+
+type PermissionCallback = (result: boolean) => void;
+
+const bleManager = new BleManager();
+
+const SERVICE_ID = 'b7ef1193-dc2e-4362-93d3-df429eb3ad10';
+// const CMD_CHARAC_ID = '00ce7a72-ec08-473d-943e-81ec27fdc600';
+const DATA_CHARAC_ID = '00ce7a72-ec08-473d-943e-81ec27fdc5f2';
+
+// const CONNECTED_DEVICE_DUMMY = {
+//   id: 1,
+//   name: 'Test Device',
+// };
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(callback: PermissionCallback): Promise<void>;
+  scanForDevices(): void;
+  allDevices: Device[];
+  isScanningDevice: boolean;
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  connectedDevice: Device | null;
+  monitoredData: string;
+}
+
+export default function useBle(): BluetoothLowEnergyApi {
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [isScanningDevice, setIsScanningDevice] = useState(false);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [monitoredData, setMonitoredData] = useState('');
+
+  const requestPermissions = async (callback: PermissionCallback) => {
+    const apiLevel = await DeviceInfo.getApiLevel();
+    console.log('apiLevel: ', apiLevel);
+    if (apiLevel < 31) {
+      const grantedStatus = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'Bluetooth Low Energy Needs Location Permission',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'Ok',
+          buttonNeutral: 'Maybe Later',
+        },
+      );
+      callback(grantedStatus === PermissionsAndroid.RESULTS.GRANTED);
+    } else {
+      const result = await PermissionsAndroid.requestMultiple([
+        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      ]);
+
+      const isAllPermissionsGranted =
+        result['android.permission.BLUETOOTH_SCAN'] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.BLUETOOTH_CONNECT'] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.ACCESS_FINE_LOCATION'] ===
+          PermissionsAndroid.RESULTS.GRANTED;
+
+      callback(isAllPermissionsGranted);
+    }
+  };
+
+  const isDuplicateDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex(device => device?.id === nextDevice?.id) > -1;
+
+  const scanForDevices = () => {
+    setIsScanningDevice(true);
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        setIsScanningDevice(false);
+        ToastAndroid.show('Error Scanning Devices', ToastAndroid.SHORT);
+      }
+      if (device) {
+        setAllDevices(prevState => {
+          if (!isDuplicateDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+        setIsScanningDevice(false);
+      }
+    });
+  };
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      const deviceConnection = await bleManager.connectToDevice(device?.id);
+      setConnectedDevice(deviceConnection);
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      bleManager.stopDeviceScan();
+      startStreamingData(device);
+    } catch (error) {
+      ToastAndroid.show('Error Connecting Device', ToastAndroid.SHORT);
+    }
+  };
+
+  const onDetectData = (
+    error: BleError | null,
+    characteristic: Characteristic | null,
+  ) => {
+    if (error) {
+      ToastAndroid.show('Error Detect Data', ToastAndroid.SHORT);
+      return;
+    } else if (!characteristic) {
+      ToastAndroid.show('No Characteristic Found', ToastAndroid.SHORT);
+      return;
+    }
+    setMonitoredData('Success Getting the Data');
+  };
+
+  const startStreamingData = async (device: Device) => {
+    if (device) {
+      device.monitorCharacteristicForService(
+        SERVICE_ID,
+        DATA_CHARAC_ID,
+        onDetectData,
+      );
+    } else {
+      ToastAndroid.show('No Device Connected', ToastAndroid.SHORT);
+    }
+  };
+
+  return {
+    requestPermissions,
+    scanForDevices,
+    allDevices,
+    isScanningDevice,
+    connectToDevice,
+    connectedDevice,
+    monitoredData,
+  };
+}
---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, Text, Button, ActivityIndicator} from 'react-native';
+
+import styles from './styles';
+import useBle from '../../hooks/useBle';
+import {Device} from 'react-native-ble-plx';
+
+const HomeScreen = () => {
+  const {
+    requestPermissions,
+    scanForDevices,
+    allDevices,
+    isScanningDevice,
+    connectToDevice,
+    connectedDevice,
+    monitoredData,
+  } = useBle();
+
+  const onScanDevices = async () => {
+    requestPermissions((isGranted: boolean) => {
+      if (isGranted) {
+        scanForDevices();
+      }
+    });
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Available Device</Text>
+        {isScanningDevice && (
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator size={'small'} />
+          </View>
+        )}
+        {allDevices?.map((device: Device) => (
+          <View style={styles.deviceItemContainer}>
+            <View style={styles.deviceItem}>
+              <Text>{device?.name}</Text>
+            </View>
+            <View style={styles.connectButtonContainer}>
+              <Button title="Connect" onPress={() => connectToDevice(device)} />
+            </View>
+          </View>
+        ))}
+        {!!connectedDevice && (
+          <View style={styles.connectedDeviceContainer}>
+            <Text style={styles.title}>Connected Device</Text>
+            <Text
+              style={
+                styles.data
+              }>{`Device Name: ${connectedDevice?.name}`}</Text>
+            <Text
+              style={styles.data}>{`Device ID: ${connectedDevice?.id}`}</Text>
+            <Text>{`Data: ${monitoredData}`}</Text>
+          </View>
+        )}
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Scan Devices" onPress={onScanDevices} />
+      </View>
+    </>
+  );
+};
+
+export default HomeScreen;
